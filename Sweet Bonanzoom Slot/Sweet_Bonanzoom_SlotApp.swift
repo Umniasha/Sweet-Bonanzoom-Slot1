f6860ae64015b368f7bce21af1265b85e64f3039
SF-testUmnyasha <newashedelo@gmail.com>
2024-04-24 12:36:06 +0300

    Initial Commit

---
+++ Sweet Bonanzoom Slot/Sweet_Bonanzoom_SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_Bonanzoom_SlotApp.swift
+//  Sweet Bonanzoom Slot
+//
+//  Created by Oleg Arnaut  on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_Bonanzoom_SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sweet Bonanzoom SlotUITests/Sweet_Bonanzoom_SlotUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sweet_Bonanzoom_SlotUITestsLaunchTests.swift
+//  Sweet Bonanzoom SlotUITests
+//
+//  Created by Oleg Arnaut  on 24.04.2024.
+//
+
+import XCTest
+
+class Sweet_Bonanzoom_SlotUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
