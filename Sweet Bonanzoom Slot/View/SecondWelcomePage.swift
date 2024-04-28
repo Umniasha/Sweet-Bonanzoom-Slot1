ea0cb4f0819eea16e972dde1f6973338213fd31a
SF-testUmnyasha <newashedelo@gmail.com>
2024-04-28 16:49:59 +0300

    update background welcome pages

--- Sweet Bonanzoom Slot/View/SecondWelcomePage.swift
+++ Sweet Bonanzoom Slot/View/SecondWelcomePage.swift
@@ -16,10 +16,7 @@ struct SecondWelcomePage: View {
         
         ZStack{
             
-            Image("SecondWelcomSceneBG")
-                .resizable()
-                .frame(width: ContentView().frameWidth, height: ContentView().frameHeight)
-                .edgesIgnoringSafeArea(.all)
+            
             
             VStack(alignment: .center){
                 
@@ -28,7 +25,6 @@ struct SecondWelcomePage: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: ContentView().frameWidth/1.4)
                     .padding([.leading,.trailing])
-                    .padding(.top, 40)
                 
                 Text("This game is a purely virtual game designed for \n entertainment purposes only. This game does \n not involve or support real money gambling, \n betting, or any monetary transactions. All in-\n game currency, referred to as \"Zooms\" and any form of winnings or bonuses, are entirely fictional and have no real-world value.\n Our game is created to provide a fun and engaging experience for our players within a safe and responsible gaming environment. It is not intended for use as a gambling platform, nor does it offer opportunities for real money gambling or the chance to win real money or real prizes. \n Players are reminded to enjoy game responsibly and within the limits of entertainment. We encourage all players to be aware of their gaming habits and to play sensibly, keeping in mind that the game is meant as a form of virtual entertainment. ")
                     .font(.custom("Maven Pro", size: 15))
@@ -56,6 +52,10 @@ struct SecondWelcomePage: View {
                 Spacer()
                 
             }
+            .background(Image("SecondWelcomSceneBG")
+                            .resizable()
+                            .frame(width: ContentView().frameWidth, height: ContentView().frameHeight)
+                            .edgesIgnoringSafeArea(.all))
         }
     }
 }
